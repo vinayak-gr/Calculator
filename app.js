37fb134d25d004629f6494f31aa64f198fa1b492
vinayak-gr <vinayak7259@gmail.com>
2025-01-06 23:26:50 +0530

    Updated function deleteToDisplay

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ function clearDisplay () {
     result.value = ""
 }
 
+function deleteToDisplay () {
+    result.value = result.value.slice(0, -1)
+}
+
 function calculate () {
     try {
         result.value = eval(result.value);
